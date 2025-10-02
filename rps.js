5f6e28ec0b20b3181d691ecb60246a661d0107f9
Samuel Horry <211251918+Sam-Horry@users.noreply.github.com>
2025-10-02 15:28:47 +0100

    first commit
    starting the project

---
+++ rps.js
@@ -0,0 +1,9 @@
+console.log("Hello world!")
+
+function getComputerChoice(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+console.log(getComputerChoice(1, 3));
