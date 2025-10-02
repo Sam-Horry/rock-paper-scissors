7e245e3b8abed060b53bbd16b6d64016f42376c8
Samuel Horry <211251918+Sam-Horry@users.noreply.github.com>
2025-10-02 15:44:20 +0100

    computer choice
    added a function for getting the computer to choose between rock paper
    and scissors.

--- rps.js
+++ rps.js
@@ -1,9 +1,19 @@
-console.log("Hello world!")
-
-function getComputerChoice(min, max) {
+function randomChoice(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-console.log(getComputerChoice(1, 3));
+function getComputerChoice(randomChoice) {
+  if (randomChoice == 1) {
+    console.log("Rock!");
+  }
+  else if (randomChoice == 2) {
+    console.log("Paper!");
+  }
+  else if (randomChoice == 3) {
+    console.log("Scissors!");
+  }
+}
+
+console.log(getComputerChoice(randomChoice(1, 3)));
